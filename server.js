261270e62ed6410418658ea9c67fedd228ad263f
varunkochar <47768616+varunkochar@users.noreply.github.com>
2020-06-16 17:05:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,14 +10,16 @@ var https_port = process.env.HTTPS_PORT || parseInt(port) + 1;
 var jsonParser = bodyParser.json();
 
 app.get("/", function(req, res) {
-  console.log("req = "+req);
-  console.log("res = "+res);
+  console.log("req = " + req);
+  console.log("res = " + res);
+  console.log("inside get " );
   res.send("Nothing chek try printing");
 });
 
 // Change the URL to an individual AND hard to guess URL
 // https://developers.google.com/hangouts/chat/how-tos/bots-develop#registering_the_bot
 app.post("/ThisIsMyHardToGuessUrl", jsonParser, function(req, res) {
+  console.log("inside post " );
   if (chat.securityCheck(req, res)) {
     chat.processChat(req.body, res);
   }
