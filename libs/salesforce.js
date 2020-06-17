deedd67caa9467fcaf4262907c987e1ae711b301
varunkochar <47768616+varunkochar@users.noreply.github.com>
2020-06-17 13:34:52 +0530

    Update salesforce.js

--- libs/salesforce.js
+++ libs/salesforce.js
@@ -44,6 +44,8 @@ exports.queryForBot = query => {
 
 exports.callApex = () =>{
    console.log("Inside call apex");
+  console.log("org "+org);
+  console.log("Iorg.apex"+org.apex);
   var body = "hiee";
   org.apex.post("/googleHackBot", body, function(err, resp) {
       console.log("Inside call apex" +res);});
